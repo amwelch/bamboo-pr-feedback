f8b07bc7767227370b68372bf1977c3feeb3e942
Sandy <amwelch@umich.edu>
2015-03-11 21:55:28 -0400

    support for authetication with gh shared secret

--- bin/pr-feedback-server.py
+++ bin/pr-feedback-server.py
@@ -4,6 +4,8 @@ import tornado.web
 import tornado.ioloop
 import requests
 import json
+import hashlib
+import hmac
 
 def get_config(config_file="../config/config.json"):
     try:
@@ -14,11 +16,37 @@ def get_config(config_file="../config/config.json"):
         print "Config file {} does not exist".format(config_file)
     return data
 
+def get_sha1_hmac(shared_secret, raw):
+     hashed = hmac.new(shared_secret, raw, hashlib.sha1)
+     return hashed.digest().encode("base64").rstrip("\n")
+
 class GithubHandler(tornado.web.RequestHandler):
     '''
     Handle posts from github hook
     '''
     bamboo_queue_build = "https://{}:{}/bamboo/rest/api/latest/queue/{}?os_athType=basic{}"
+    def verify_secret(self, request):
+        '''
+        Verify the shared secret, returns True on verified False otherwise
+        '''
+        config = get_config()
+        ss = config.get("github_shared_secret")
+        if not ss:
+            print "No shared secret configured (github_shared_secret)"
+            return
+        gh_digest = request.headers.get("X-Hub-Signature")
+        if not gh_digest:
+            print "Did not recieve digest from github. Do you have it configured in the hook?"
+            return
+        local_digest = get_sha1_hmac(request.body)
+        if local_digest != gh_digest:
+            print "Digest from github did not match our digest"
+            print "GH   : {}".format(gh_digest)
+            print "LOCAL: {}".format(local_digest)
+            return
+        else:
+            return True
+
     def post(self):
 
         data = self.request.body
@@ -34,7 +62,6 @@ class GithubHandler(tornado.web.RequestHandler):
         port = config.get("bamboo_port", 443)
         user = config.get("bamboo_user")
         password = config.get("bamboo_password")       
-
 
         bamboo_data = {}
         bamboo_data["pull_num"] = data.get("pull_num")
