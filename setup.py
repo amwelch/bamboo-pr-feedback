fb58d20ea06683879cbc3e94104218f57c4d37d4
Sandy <amwelch@umich.edu>
2015-03-12 21:47:16 -0400

    Added requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, Command
+
+setup(name='bamboo-pr',
+      version='1.0',
+      description='Middleware to get bamboo status on github pullrequests',
+      author='Alexaeder Welch',
+      author_email='amwelch3@gmail.com',
+      packages=['bamboo-pr'],
+     )
