9a69ab491ac45937ff2515f2a8450d1ffce3b782
Sandy <amwelch@umich.edu>
2015-05-20 00:49:33 +0000

    fix pr server name

--- bin/test-client.py
+++ bin/test-client.py
@@ -10,14 +10,13 @@ import pr_feedback_server
 
 def main():
     config_file = os.environ.get('BAMBOO_PR_FEEDBACK_CONFIG', "../config/config.json")
-    config = pr-feedback-server.get_config(config_file)
+    config = pr_feedback_server.get_config(config_file)
 
     bamboo_data = {}
-    bamboo_data['pull_num'] = '3'
+    bamboo_data['pull_num'] = '530'
     bamboo_data['pull_sha'] = 'foo'
     bamboo_data['author'] = 'bar'
    
-
     pr_feedback_server.run_bamboo_job(config['plan'], config['host'], config['port'],
         config['user'], config['password'], bamboo_data)
 
