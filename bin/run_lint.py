4deaa4c922ae65a764a14c5e95eff193ac374588
Sandy <amwelch@umich.edu>
2015-10-12 17:02:41 +0000

    Push feedback about actual files on a per commit basis

--- bin/run_lint.py
+++ bin/run_lint.py
@@ -12,7 +12,7 @@ def post_result(base, failed_files, api_key, sha):
                                                                  api_key=api_key)
     if any(failed_files):
         status = 'failure'
-        description = 'Lint failed for: ' + " ".join(failed_files)
+        description = 'Lint failed'
         # Sad Panda
         link = 'http://fc08.deviantart.net/fs70/f/2010/149/a/7/Sad_Panda_Chibi_by_mongrelssister.png'
     else:
@@ -26,16 +26,47 @@ def post_result(base, failed_files, api_key, sha):
     requests.post(url, data=json.dumps(data), headers=headers)
 
 
+def post_errors(errors, base, api_key, sha):
+    url = base + '/commits/{sha}/comments?access_token={api_key}'.format(sha=sha,
+                                                                         api_key=api_key)
+    for fname, errors in errors.iteritems():
+        msg = ['Lint Errors for {}'.format(fname)]
+        for error in errors:
+            line_num, errstr = error
+            msg.append('\t{}:\t{}'.format(line_num, errstr))
+        msg = '\n'.join(msg)
+        data = {'body': msg,
+                'path': fname,
+                'position': 1,
+                'line': None}
+        headers = {}
+        headers['Accept'] = 'application/json'
+        requests.post(url, data=json.dumps(data), headers=headers)
+
+
+def get_errors(lint_output):
+    errors = {}
+    for line in lint_output:
+        if line:
+            #  lib/deepy/jobs_daemon.py:7: 'foo' imported but unused
+            fname, num, errstr = line.split(':')
+            errors.setdefault(fname, [])
+            errors[fname].append((num, errstr))
+    return errors
+
+
 def run_lint(path, files, lint='pyflakes', pattern='*.py'):
     failed = []
+    output = []
     for fname in files:
         if not fnmatch.fnmatch(fname, pattern):
             continue
         try:
-            subprocess.check_call([lint + " " + fname], shell=True, cwd=path)
-        except subprocess.CalledProcessError:
+            subprocess.check_output([lint + " " + fname], shell=True, cwd=path)
+        except subprocess.CalledProcessError as e:
+            output += e.output.split('\n')
             failed.append(fname)
-    return failed
+    return failed, output
 
 
 def get_changed_files(api_key, repo_base, pr_num):
@@ -51,7 +82,7 @@ def get_changed_files(api_key, repo_base, pr_num):
 
 
 def parse_args():
-    p = argparse.ArgumentParser(description ='''
+    p = argparse.ArgumentParser(description='''
     Get changed files from a pull-request and check for lint errors
     ''')
     p.add_argument('--pr-num', help='pull-request num', required=True)
@@ -65,7 +96,9 @@ def parse_args():
 def main():
     args = parse_args()
     files = get_changed_files(args.gh_api, args.repo_base, args.pr_num)
-    failed = run_lint(args.path, files)
+    failed, errors = run_lint(args.path, files)
+    errors = get_errors(errors)
+    post_errors(errors, args.repo_base, args.gh_api, args.sha)
     post_result(args.repo_base, failed, args.gh_api, args.sha)
 
 if __name__ == '__main__':
